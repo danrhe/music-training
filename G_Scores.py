4d3adcf17e014f402cad7ba4aaae4e51027ab2da
danrhe <daniel.vonrhein@gmail.com>
2016-02-09 08:12:41 +0100

    Uploading raw experiment

---
+++ G_Scores.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+A simple training for sheets music reading
+
+"""
+
+from expyriment import design, control, stimuli
+from settings import *
+from expyriment.misc import constants
+import random
+control.defaults.open_gl = False # switch off opengl to avoid screen refesh sync
+control.set_develop_mode(True)
+
+exp = design.Experiment(name="G_Scores")
+control.initialize(exp)
+
+
+nTrials = 5
+
+
+# Create a canvas and add a clef and white lines to form the music sheet
+music_sheet = stimuli.Canvas(settings_canvas['screen_size'], colour=settings_canvas['colour'])
+
+clef = stimuli.Picture('Stimuli/notenschluessel2.jpg',position=[-150, y_init - (2 * line_dist)])
+clef.scale(.08)
+clef.plot(music_sheet)
+
+for item in lines:
+    line = stimuli.Line(item['start_point'],
+                    item['end_point'],
+                    item['line_width'],
+                    colour=item['colour'])
+    line.plot(music_sheet)
+
+
+notes = []
+
+for item in mapping:
+    notes.append(note(*item.values()))
+
+control.start()
+
+
+for iTrial in range(0, nTrials):
+
+    # get the index of one note - key mapping
+    iMapping = random.randint(0, len(notes))
+
+
+    # Clear the screen
+    stimuli.BlankScreen().present(clear=True, update=False)
+
+    # Present the sheet
+    music_sheet.present(clear=False, update=True)
+
+    # Add the note to the sheet
+    notes[iTrial].stimuli.present(clear=False, update=True)
+
+
+    # Wait for button press
+    exp.keyboard.wait(eval("constants.K_" + notes[iTrial].keyboard))
+
+    # Add feedback to the screen
+    text_mapping = stimuli.TextBox(notes[iTrial].key, [80, 80], position=[0, (y_init + line_dist)], text_size=18)
+    text_mapping.present(clear=False,update=True)
+    exp.keyboard.wait(constants.K_SPACE)
+
+control.end(goodbye_text='Thats it', goodbye_delay=1000)
+
---
+++ settings.py
@@ -0,0 +1,65 @@
+from expyriment import stimuli
+############
+# Settings #
+############
+line_dist = 30
+white = [255, 255, 255]
+
+class note:
+    def __init__(self, position,key, keyboard='K_SPACE',colour=None):
+
+        if colour is not None:
+            self.colour = colour
+        else:
+            self.colour = white
+
+        # Assign
+        self.key = key
+        self.position = position
+        self.keyboard = keyboard
+
+        # standard values for each note
+        self.size = [line_dist, line_dist / 2]
+        self.stimuli = stimuli.Ellipse(self.size, colour=self.colour, position=self.position)
+
+        self.count = 0
+        self.RTs = []
+        self.misses = 0
+
+
+settings_canvas = {
+    #[SCREEN_WIDTH, SCREEN_HEIGHT]}
+    'screen_size': [1680, 1050],
+    'colour': [0, 0, 0],
+    }
+
+y_init = 120
+line_dist = 30
+
+lines = list()
+for i in range(-2,2,1):
+    lines.append(
+        {
+        'start_point': [-110, y_init - (i * line_dist)],
+        'end_point': [110, y_init - (i * line_dist)],
+        'line_width': 3,
+        'colour': white,
+        })
+
+mapping = [
+    {'key': 'f2',
+     'position': [0, y_init],
+     'keyboard': 'f'
+     },
+    {'key': 'e2',
+     'position': [0, y_init - (1 * line_dist / 2)],
+     'keyboard': 'e'
+     },
+    {'key': 'e1',
+     'position': [0, y_init - (8 * line_dist / 2)],
+     'keyboard': 'e'
+     },
+]
+
+
+
