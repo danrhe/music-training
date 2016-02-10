452fac297c4ecd5c6a0ca8aa3ca04a56b4f57fb3
danrhe <daniel.vonrhein@gmail.com>
2016-02-10 14:08:08 +0100

    Added all additional notes from bottom till top line

--- settings.py
+++ settings.py
@@ -24,6 +24,30 @@ mapping = [
      'position': [0, y_init + (3 * line_dist / 2)],
      'keyboard': 'e'
      },
+    {'key': 'd2',
+     'position': [0, y_init + (2 * line_dist / 2)],
+     'keyboard': 'd'
+     },
+    {'key': 'c2',
+     'position': [0, y_init + (1 * line_dist / 2)],
+     'keyboard': 'c'
+     },
+    {'key': 'b1',
+     'position': [0, y_init + (0 * line_dist / 2)],
+     'keyboard': 'b'
+     },
+    {'key': 'a1',
+     'position': [0, y_init + (-1 * line_dist / 2)],
+     'keyboard': 'a'
+     },
+    {'key': 'g1',
+     'position': [0, y_init + (-2 * line_dist / 2)],
+     'keyboard': 'g'
+     },
+    {'key': 'f1',
+     'position': [0, y_init + (-3 * line_dist / 2)],
+     'keyboard': 'f'
+     },
     {'key': 'e1',
      'position': [0, y_init + (-4 * line_dist / 2)],
      'keyboard': 'e'
