fb3fda6e7b00d62faa1247fd0a21c460887fd55e
danrhe <daniel.vonrhein@gmail.com>
2016-03-01 11:17:52 +0100

    Fixed Test file

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from expyriment import design, control,stimuli
+from expyriment import design, control,stimuli,io
 from expyriment.misc import constants
 from functions2 import MusicSheet
 '''
@@ -8,8 +8,8 @@ Custom settings
 control.defaults.open_gl = False
 
 # Fast open close and windowed; default: False
-#control.set_develop_mode(True)
-control.defaults.initialize_delay = 0
+control.set_develop_mode(True)
+#control.defaults.initialize_delay = 0
 
 '''
 Prepare experiment
@@ -20,14 +20,17 @@ exp = design.Experiment(name="MusicTraining")
 control.initialize(exp)
 
 
-musicsheet = MusicSheet(screen_size=exp.screen.size)
+musicsheet = MusicSheet(screen_size=exp.screen.size, pos_y={'g': 0, 'f': 0})
 
 control.start(subject_id=45)
 
 stimuli.BlankScreen().present(clear=True, update=False)
 
 musicsheet.Field.present(clear=False, update=True)
+mouse = io.Mouse()
+mouse.show_cursor()
 # Wait for button press
 key, rt = exp.keyboard.wait(constants.K_ALL_LETTERS)
+
 
 control.end(goodbye_text='Thats it', goodbye_delay=1000)
