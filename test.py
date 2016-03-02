cca62bd6a8f74e02fe384293ddec0fdea89e3d11
danrhe <daniel.vonrhein@gmail.com>
2016-03-02 13:37:20 +0100

    added function showing if mouse is within defined field

--- test.py
+++ test.py
@@ -20,15 +20,44 @@ exp = design.Experiment(name="MusicTraining")
 control.initialize(exp)
 
 
-musicsheet = MusicSheet(screen_size=exp.screen.size, pos_y={'g': 0, 'f': 0})
+rect = stimuli.Rectangle([20,40],[200,0,0], position=[20,20])
 
 control.start(subject_id=45)
 
 stimuli.BlankScreen().present(clear=True, update=False)
 
-musicsheet.Field.present(clear=False, update=True)
+rect.present(clear=False, update=True)
 mouse = io.Mouse()
 mouse.show_cursor()
+
+for i in range(0, 10000):
+
+    position = rect.position
+    size = rect.size
+
+    mp = mouse.position
+
+    right = (size[0] / 2) + position[0]
+    left = right - size[0]
+    top = (size[1] / 2) + position[1]
+    bottom = top - size[1]
+
+    if mp[0] <= right and mp[0] >=  left and mp[1] <= top and mp[1] >= bottom:
+        t = 'TRUE'
+    else:
+        t = 'FALSE'
+
+    text = stimuli.TextLine(t, [0,-100], text_colour=[200,200,0])
+    text2 = stimuli.TextLine("R: " + str(right) + " L: " + str(left) + " T: " + str(top) + "B: " + str(bottom), [0,-150], text_colour=[200,200,0])
+    text3 = stimuli.TextLine("mp[0]: " + str(mp[0]) + " mp[1]: " + str(mp[1]), [0,-200], text_colour=[200,200,0])
+
+
+    text.present(clear=True, update=False )
+    text2.present(clear=False, update=False )
+    text3.present(clear=False, update=False )
+    rect.present(clear=False, update=True)
+
+
 # Wait for button press
 key, rt = exp.keyboard.wait(constants.K_ALL_LETTERS)
 
